ab8bc984ed1e9d4e79789523c0b5c3d6521c0a6d
Luke Shirnia <lukeshirnia@btinternet.com>
2017-03-08 17:28:52 +0000

    Update oom.py

--- oom.py
+++ oom.py
@@ -1,8 +1,29 @@
 from sys import argv
-import copy
 
-total_rss = []
 script, LOG_FILE = argv
+total_rss = []
+
+def print_header():
+
+        print "-" * 40
+        print ""
+        print "      _____ _____ _____ "
+        print "     |     |     |   _ |"
+        print "     |  |  |  |  | | | |"
+        print "     |_____|_____|_|_|_|"
+        print ""
+        print "     Out Of Memory Analyser"
+        print ""
+        print "-" * 40
+        print ""
+
+def system_resources():
+   with open("/proc/meminfo", "r") as meminfo:
+      for lines in meminfo:
+         if "MemTotal" in lines.strip():
+            memory_value = int(lines.split()[1])
+            system_memory = memory_value / 1024
+            return system_memory
 
 def strip_rss(line):
    rss_to_add = []
@@ -22,7 +43,8 @@ def strip_line(line):
 def calc_rss(adding_rss):
    total = sum(adding_rss)
    return total
-    
+
+
 with open(LOG_FILE, "r") as inLogFile, open("/home/rack/oom", "w") as outfile:
   record = False
   for line in inLogFile:
@@ -37,5 +59,10 @@ with open(LOG_FILE, "r") as inLogFile, open("/home/rack/oom", "w") as outfile:
       total_rss.append(rss_value)
 
 
+print_header()
+system_rss = system_resources()
+print "Total System Memory:                 %s MB" % (system_rss)
 total_ram = ( add_rss(total_rss) * 4 ) / 1024
-print "Total RAM usage at OOM incident = %s MB" % (total_ram)
+print "Estimated RAM usage at OOM incident: %s MB" % (total_ram)
+print ""
+print "-" * 40
