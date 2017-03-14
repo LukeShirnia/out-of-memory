88faa155460085cb3f29b4907ec81741386d19c5
Luke Shirnia <lukeshirnia@btinternet.com>
2017-03-14 11:35:09 +0000

    Update oom.py

--- oom.py
+++ oom.py
@@ -5,6 +5,7 @@
 from sys import argv
 import platform
 import re
+import gzip
 
 total_individual = []
 CentOS_RedHat_Distro = ['redhat', 'centos']
@@ -53,8 +54,9 @@ def system_resources():
             system_memory = memory_value / 1024
             return system_memory
 
-def strip_rss(line):
-   value = int(line.split()[10])
+def strip_rss(line, column_number):
+   line = line.split()
+   value = int(line[column_number-1])
    return value
 
 def add_rss(total_rss):
@@ -73,7 +75,7 @@ def check_if_incident(counter, oom_date_count, total_rss_per_incident, killed_se
                 # print "Dates OOM Occured"
                 for i in range(1, counter):
                         print "-" * 40
-                        print "Dates OOM occured:    %s" % (", ".join(oom_date_count[i - 1]))
+                        print "Date OOM occured:    %s" % (", ".join(oom_date_count[i - 1]))
                         print "Sytem RAM: {0:14} MB".format(system_resources())
                         print "Estimated RAM at OOM: %s MB" % (sum(total_rss_per_incident[i] * 4 ) / 1024)
                         print "Services Killed:      %s " % (", ".join(killed_services[i]))
@@ -90,18 +92,18 @@ def check_if_incident(counter, oom_date_count, total_rss_per_incident, killed_se
                 print "OOM has NOT occured recently!"
                 print ""
 
-def service_count(line):
-        value = line.split()[-1:]
-        if len(value) == 1:
-                return value
+#def service_count(line):
+#        value = line.split()[-1:]
+#        if len(value) == 1:
+#                return value
 
 
 # this function processes each line and saves the rss value and process name .eg (51200, apache)
-def save_values(line):
+def save_values(line, column_number):
         value = line.split()[-1:]
         if len(value) == 1:
                 cols = line.split()
-                string = cols[10], cols[-1]
+                string = cols[column_number-1], cols[-1]
                 return string
 
 # this function takes the full list of saved processes and find each unique occurance of a process
@@ -130,6 +132,21 @@ def add_rss_for_processes(unique, list_of_values):
         return total_service_usage
 
 
+def openfile(filename, normal_file):
+	if normal_file:
+		return open(filename, "r")
+	elif filename.endswith('.gz'):
+        	return gzip.open(filename, "r")
+	else:
+		return open(filename, "r")
+
+def find_rss_column(line):
+	for i, word in enumerate(line):
+		if word == "rss":
+			column = int(i+1)
+			return column
+
+
 def OOM_record(LOG_FILE):
   oom_date_count = []
   running_service = []
@@ -139,44 +156,48 @@ def OOM_record(LOG_FILE):
   unique_services = {}
   service_value_list = {}
   total_service_list = []
-  with open(LOG_FILE, "r") as inLogFile: #, open("/home/rack/oom", "w") as outfile:
-    record = False
-    record_oom_true_false = False
-    counter = 1
-    for line in inLogFile:
-      killed = re.search("Killed process (.*) total", line)
-      if "[ pid ]   uid  tgid total_vm      rss" in line.strip():
-        total_rss[counter] = []
-        killed_services[counter] = []
-        unique_services[counter] = []
-        list_of_values[counter] = []
-        record = True
-        record_oom_true_false = False
-        oom_count_plus_one = line.split()[0:3]
-        oom_date_count.append(oom_count_plus_one)
-      elif "Out of memory: Kill process" in line.strip():
-        service_value_list[counter] = []
-        list_of_values[counter] = filter(None, list_of_values[counter])
-        unique = find_unique_services(list_of_values[counter])
-        oom_services = add_rss_for_processes(unique, list_of_values[counter])
-        oom_services = sorted(oom_services,key=lambda x: x[1], reverse=True)
-        service_value_list[counter] = oom_services
-        service_value_list[counter] = service_value_list[counter][:5]
-        record_oom_true_false = True
-        record = False
-        counter += 1
-      elif record:
-        line = strip_line(line)
-        count_service = service_count(line)
-        list_of_values[counter].append(save_values(line)) #service rss calulation initiation
-        rss_value = strip_rss(line)
-        total_rss[counter].append(rss_value) # calculate total value of all processes
-      elif record_oom_true_false and killed:
-        killed = killed.group(1)
-        killed = strip_line(killed)
-        killed = killed.strip("0123456789 ")
-        killed_services[counter-1].append(killed)
-    check_if_incident(counter, oom_date_count, total_rss, killed_services, service_value_list)
+  normal_file = (False if LOG_FILE.endswith('.gz') else True)
+  inLogFile = openfile(LOG_FILE, normal_file) #, open("/home/rack/oom", "w") as outfile:
+  record = False
+  record_oom_true_false = False
+  counter = 1
+  for line in inLogFile:
+    killed = re.search("Killed process (.*) total", line)
+    if "[ pid ]   uid  tgid total_vm      rss" in line.strip():
+      total_rss[counter] = []
+      killed_services[counter] = []
+      unique_services[counter] = []
+      list_of_values[counter] = []
+      record = True
+      record_oom_true_false = False
+      oom_count_plus_one = line.split()[0:3]
+      oom_date_count.append(oom_count_plus_one)
+      line = strip_line(line)
+      column_number = find_rss_column(line.split())
+    elif "Out of memory: Kill process" in line.strip() or len(line) < 16 and record == True:
+      service_value_list[counter] = []
+      list_of_values[counter] = filter(None, list_of_values[counter])
+      unique = find_unique_services(list_of_values[counter])
+      oom_services = add_rss_for_processes(unique, list_of_values[counter])
+      oom_services = sorted(oom_services,key=lambda x: x[1], reverse=True)
+      service_value_list[counter] = oom_services
+      service_value_list[counter] = service_value_list[counter][:5]
+      record_oom_true_false = True
+      record = False
+      counter += 1
+    elif record:
+      line = strip_line(line)
+      #count_service = service_count(line)
+      list_of_values[counter].append(save_values(line, column_number)) #service rss calulation initiation
+      rss_value = strip_rss(line, column_number)
+      total_rss[counter].append(rss_value) # calculate total value of all processes
+    elif record_oom_true_false and killed:
+      killed = killed.group(1)
+      killed = strip_line(killed)
+      killed = killed.strip("0123456789 ")
+      killed_services[counter-1].append(killed)
+  inLogFile.close()
+  check_if_incident(counter, oom_date_count, total_rss, killed_services, service_value_list)
 
 print_header()
 os_check_value = os_check()
@@ -186,7 +207,6 @@ if len(argv) == 1:
                 system_rss = system_resources()
                 OOM_record("/var/log/messages")
         elif os_check_value.lower() in Ubuntu_Debian_Distro:
-                #print "Ubuntu"
                 OOM_record("/var/log/syslog")
         else:
                 print "Unsupported OS"
@@ -196,7 +216,6 @@ elif len(argv) == 2:
                 system_rss = system_resources()
                 OOM_record(OOM_LOG)
         elif os_check_value.lower() in Ubuntu_Debian_Distro:
-                #print "Ubuntu"
                 OOM_record(OOM_LOG)
         else:
                 print "Unsupported OS"
